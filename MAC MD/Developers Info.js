ba985007c24518aee42c9a167d0b002c1be98acc
emmanuelfosso205 <166316042+emmanuelfosso205@users.noreply.github.com>
2024-06-09 15:09:12 +0100

    Update Developers Info.js

--- MAC MD/Developers Info.js
+++ MAC MD/Developers Info.js
@@ -1,4 +1,4 @@
 
-Developer one = Mas tech https://wa.me/256705036288
+Developer one = Mas tech https://wa.me/237674051182
 
-Developer two = IBRAHIM TECH: https://wa.me/254710772666
+Developer two = IBRAHIM TECH: https://wa.me/237675158645
